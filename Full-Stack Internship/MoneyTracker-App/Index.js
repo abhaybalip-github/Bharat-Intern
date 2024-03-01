d93fc567faf23c74031aab58837b83b93d4cbdc2
Abhay Balip <abhay.balip@gmail.com>
2024-03-01 22:34:06 +0530

    file

--- Full-Stack Internship/MoneyTracker-App/Index.js
+++ Full-Stack Internship/MoneyTracker-App/Index.js
@@ -1,2 +1,23 @@
-const print = (data)=> console.log(data)
+const print = (data) => console.log(data)
 
+const express = require('express')
+const http = require('http')
+const fs = require('fs')
+
+const app = express()
+app.use(express.static(__dirname + "/Asset"))
+
+app.all('/', function (req, res) {
+    if (req.method == 'GET') {
+        res.sendFile(__dirname + "/Asset/Index.html")
+    }
+    else { res.send('Error Occured !') }
+})
+
+app.listen(1000,function(err){
+    if(err){
+        print("Error :"+err)
+    }else{
+        print("server started")
+    }
+})
--- Full-Stack Internship/SignUp-App/Index.js
+++ Full-Stack Internship/SignUp-App/Index.js
@@ -1,45 +1,52 @@
-// Node js
-
 const print = (data) => console.log(data)
 
+const http = require('http')
 const express = require('express')
-const nodemon = require('nodemon')
-const fs = require('fs')
-const mongo = require('mongodb')
 const exp = require('constants')
-const { emitWarning } = require('process')
+const { mongoClient } = require('mongodb')
 
-function store_data(data){
-    var cli = mongo.MongoClient
-    cli.connect('localhost:27017',{ useNewUrlParser: true, useUnifiedTopology: true },function(err,client){
-        
-    })
+// mongodb database info :
+const uri = "mongodb://localhost:27017/userData"
+const dbName = "userData"
+const collectionName = "user"
+async function StoreData(data) {
+    try {
+        const client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        const db = client.db(dbName)
+        const collection = db.collection(collectionName);
+
+        const result = await collection.insertOne(data)
+        print(`Data Inserted to DB : ${result.insertedId}`);
+
+    } catch (error) {
+        print(`Error Occured : ${error}`)
+    } finally {
+        await client.close();
+    }
 }
-
 const app = express()
+app.use(express.static(__dirname + '/Asset'))
+app.use(express.json())
+app.use('/signup', function (req, res) {
+    if (!req.errored) {
+        if (req.method == 'GET') {
+            res.sendFile(__dirname + '/Asset/Index.html')
+        }
+        else if (req.method == 'POST') {
+            res.sendFile(__dirname + '/Asset/Thanks.html')
 
-app.use('/', express.static(__dirname + "/Asset",))
-
-app.get('/', function (req, res) {
-    if (req.errored) {
-        print('error occure !')
-        app.close()
-    }
-    else {
-        res.sendFile(__dirname + "/Asset/Index.html");
-    }
-})
-
-app.use(express.urlencoded())
-
-app.post('/thanks',function(req,res){
-    if(! req.errored){
-        print(req.body)
-        store_data(req.body)
-        res.sendFile(__dirname+'/Asset/Thanks.html')
+            data = req.body
+            StoreData(data)
+        }
+    } else {
+        res.send('Error Occured !')
     }
 })
 
 app.listen(1000, function (err) {
-    print('server started')
+    print('server started !')
 })
