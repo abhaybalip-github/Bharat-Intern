9aaa88b82c40fd6902cae6dcb00bb42c764574cc
Abhay Balip <abhay.balip@gmail.com>
2024-03-10 13:46:04 +0530

    code added

--- Full-Stack Internship/MoneyTracker-App/Index.js
+++ Full-Stack Internship/MoneyTracker-App/Index.js
@@ -1,23 +1,43 @@
-const print = (data) => console.log(data)
-
 const express = require('express')
-const http = require('http')
-const fs = require('fs')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const app = express()
-app.use(express.static(__dirname + "/Asset"))
+const app = express();
 
-app.all('/', function (req, res) {
-    if (req.method == 'GET') {
-        res.sendFile(__dirname + "/Asset/Index.html")
+mongoose.connect('mongodb://localhost:27017/money-tracker', { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err))
+
+const transactionSchema = new mongoose.Schema({
+    type: String,
+    amount: Number,
+    description: String,
+    createdAt: { type: Date, default: Date.now }
+});
+const Transaction = mongoose.model('Transaction', transactionSchema)
+
+app.use(bodyParser.json());
+app.post('/add-transaction', async (req, res) => {
+    try {
+        const transaction = new Transaction(req.body)
+        const savedTransaction = await transaction.save()
+        res.json(savedTransaction)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error adding transaction')
     }
-    else { res.send('Error Occured !') }
+})
+
+app.get('/get-transactions', async (req, res) => {
+    try {
+        const transactions = await Transaction.find()
+        res.json(transactions)
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error retrieving transactions')
+    }
 })
 
 app.listen(1000,function(err){
-    if(err){
-        print("Error :"+err)
-    }else{
-        print("server started")
-    }
+    if(err) print("Error :"+err)
 })
